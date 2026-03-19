5e7b9013c40d76fb0013b31674c8c9f5267b69d2
Moh sahal <mohammedsahal1243@gmail.com>
2026-03-19 19:50:53 +0530

    chore: remove Google authentication files and dependencies

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,5 +1,4 @@
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const GitHubStrategy = require("passport-github2").Strategy;
 const User = require("../models/User");
 const { generateUniqueStudentId } = require("../helpers/studentIdGenerator");
@@ -16,86 +15,6 @@ passport.deserializeUser(async (id, done) => {
     done(err, null);
   }
 });
-
-// Google Strategy
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: (process.env.BACKEND_URL || "http://localhost:5000") + "/auth/google/callback",
-      passReqToCallback: true,
-    },
-    async (req, accessToken, refreshToken, profile, done) => {
-      try {
-        let user = await User.findOne({ 
-          $or: [
-            { "google.profileId": profile.id }, 
-            { userEmail: profile.emails[0].value }
-          ] 
-        });
-
-        if (user) {
-          // Update google info if not already set or changed
-          user.google.connected = true;
-          user.google.profileId = profile.id;
-          user.google.access_token = accessToken;
-          user.provider = user.provider || 'google';
-          user.providerId = profile.id;
-          if (profile.photos && profile.photos.length > 0) {
-            user.avatar = user.avatar || profile.photos[0].value;
-          }
-          await user.save();
-          user.isNewUser = false;
-          return done(null, user);
-        }
-
-        const mode = req.query.state || 'signin';
-        if (mode === 'signin') {
-          return done(null, false, { message: 'not_registered' });
-        }
-
-        // Create new user
-        const studentId = await generateUniqueStudentId();
-        
-        // Generate a valid userName (alphabets and spaces only)
-        let baseUserName = profile.displayName.replace(/[^A-Za-z\s]/g, '').trim();
-        if (baseUserName.length < 3) baseUserName = "LMS User";
-        if (baseUserName.length > 25) baseUserName = baseUserName.substring(0, 25);
-        
-        // Ensure uniqueness by appending numbers if needed
-        let userName = baseUserName;
-        let counter = 1;
-        while (await User.findOne({ userName })) {
-          userName = `${baseUserName} ${counter}`;
-          counter++;
-        }
-
-        user = new User({
-          userName,
-          userEmail: profile.emails[0].value,
-          role: "user",
-          avatar: profile.photos && profile.photos.length > 0 ? profile.photos[0].value : "",
-          provider: "google",
-          providerId: profile.id,
-          google: {
-            connected: true,
-            profileId: profile.id,
-            access_token: accessToken,
-          },
-          studentId: studentId,
-          isEmailVerified: true
-        });
-
-        await user.save();
-        user.isNewUser = true;
-        done(null, user);
-      } catch (err) {
-        done(err, null);
-      }
-    }
-  )
-);
 
 // GitHub Strategy
 passport.use(
